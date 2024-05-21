87b445e9f716b7e29686467f64746afc103633b9
Nadezhda Ilina <nadiailina9@gmail.com>
2024-05-21 15:20:08 +0300

    графика

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import './style.css';
 
 //Сцена
@@ -7,111 +8,77 @@ const canvas = document.querySelector('.canvas');
 
 //Камера
 const sizes = {
-  width: 600,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const cursor = {
+  x: 0,
+  y: 0,
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 scene.add(camera);
 
 //Объект
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-const group = new THREE.Group();
-const meshes = [];
-const colors = ['#ffff00', '#ff00ff', '#0000ff'];
+const material = new THREE.MeshBasicMaterial({
+  color: 'yellow',
+  wireframe: true,
+});
 
-for (let x = -1.2; x <= 1.2; x = x + 1.2) {
-  for (let y = -1.2; y <= 1.2; y = y + 1.2) {
-    const material = new THREE.MeshBasicMaterial({
-      color: colors[((Math.random() * 3) | 0) + 1],
-      wireframe: true,
-    });
+const mesh = new THREE.Mesh(geometry, material);
 
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.scale.set(0.5, 0.5, 0.5);
-    mesh.position.set(x, y, 0);
-    meshes.push(mesh);
-  }
-}
-
-group.add(...meshes);
-scene.add(group);
+scene.add(mesh);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-const clock = new THREE.Clock();
-const MAX_SCALE = 1;
-const MIN_SCALE = 0.5;
-let grow = false;
+window.addEventListener('mousemove', (e) => {
+  cursor.x = -(e.clientX / sizes.width - 0.5);
+  cursor.y = e.clientY / sizes.height - 0.5;
+});
 
-const animate = () => {
-  const delta = clock.getDelta();
+const tick = () => {
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = Math.cos(cursor.y * Math.PI * 2) * 2;
 
-  meshes.forEach((item, index) => {
-    const mult = index % 2 === 0 ? 1 : -1;
-    item.rotation.x += delta * mult;
-    item.rotation.y += delta * mult * 0.4;
-  });
+  // camera.lookAt(mesh.position);
 
-  const epalpsed = clock.getElapsedTime();
-  camera.position.x = Math.sin(epalpsed);
-  camera.position.y = Math.cos(epalpsed);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-  const mult = grow ? 1 : -1;
-  group.scale.x += mult * delta * 0.2;
-  group.scale.y += mult * delta * 0.2;
-  group.scale.z += mult * delta * 0.2;
-
-  if (grow && group.scale.x >= MAX_SCALE) {
-    grow = false;
-  } else if (group.scale.x <= MIN_SCALE) {
-    grow = true;
-  }
+  controls.update();
 
   renderer.render(scene, camera);
-  window.requestAnimationFrame(animate);
+  window.requestAnimationFrame(tick);
 };
 
-animate();
+tick();
 
-// const clock = new THREE.Clock();
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-// const tick = () => {
-//   const elepsedTime = clock.getElapsedTime();
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-//   mesh.position.x = Math.cos(elepsedTime);
-//   mesh.position.y = Math.sin(elepsedTime);
-//   camera.lookAt(mesh.position);
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
+});
 
-//   renderer.render(scene, camera);
-//   window.requestAnimationFrame(tick);
-// };
-
-// tick();
-
-// const group = new THREE.Group();
-// group.scale.y = 1.4;
-// group.rotation.x = Math.PI * 0.25;
-// const cubel1 = new THREE.Mesh(geometry, material);
-// cubel1.position.x = -1.2;
-// group.add(cubel1);
-// scene.add(group);
-
-// mesh.position.x = -1;
-// mesh.scale.x = 0.5;
-// mesh.rotation.x = Math.PI * 0.25;
-// mesh.rotation.reorder('YXZ');
-
-// const axesHelper = new THREE.AxesHelper(3);
-
-// scene.add(axesHelper);
-
-// camera.lookAt(mesh.position);
-// mesh.rotation.y = elepsedTime;
+window.addEventListener('dblclick', () => {
+  if (document.fullscreenElement) {
+    //Close
+    document.exitFullscreen();
+  } else {
+    //Open
+    canvas.requestFullscreen();
+  }
+});
